9dd4a6c5f2f7d32806812ca771f29e2f32e626de
Aeldro <nodepuydt@gmail.com>
2024-06-12 18:37:58 +0200

    It works

--- BuySellActions/Program.cs
+++ BuySellActions/Program.cs
@@ -6,41 +6,18 @@
         {
             static int maxBenef(int[] input)
             {
-
-                int[,] benefArray = new int[input.Length, input.Length];
-
-                for (int i = 0; i < input.Length; i++)
+                int addition = 0;
+                for (int i = 0; i < input.Length - 1; i++)
                 {
+                    if (input[i] < input[i + 1])
                     {
-
-                        for (int j = i; j < input.Length; j++)
-                        {
-                            {
-                                benefArray[i, j] = input[j] - input[i];
-                            }
-                        }
-
+                        addition = addition + (input[i + 1] - input[i]);
                     }
                 }
-
-                for (int i = 0; i < benefArray.GetLength(0); i++)
-                {
-                    for (int j = 0; j < benefArray.GetLength(1); j++)
-                    {
-                        if (j == benefArray.GetLength(1) - 1)
-                        {
-                            Console.WriteLine(benefArray[i, j]);
-                        }
-                        else
-                        {
-                            Console.Write(benefArray[i, j] + "        ");
-                        }
-                    }
-                }
-                return 0;
+                return addition;
             }
 
-            Console.WriteLine(maxBenef([7, 1, 5, 3, 6, 4]));
+            Console.WriteLine(maxBenef([7, 6, 4, 3, 1]));
         }
     }
 }
