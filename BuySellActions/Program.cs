1e66f566a2b72ed3187059768d363b226b371ffc
Aeldro <nodepuydt@gmail.com>
2024-06-12 12:03:15 +0200

    Ajoutez des fichiers projet.

---
+++ BuySellActions/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace BuySellActions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static int maxBenef(int[] input)
+            {
+
+                int[,] benefArray = new int[input.Length, input.Length];
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    {
+
+                        for (int j = i; j < input.Length; j++)
+                        {
+                            {
+                                benefArray[i, j] = input[j] - input[i];
+                            }
+                        }
+
+                    }
+                }
+
+                for (int i = 0; i < benefArray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < benefArray.GetLength(1); j++)
+                    {
+                        if (j == benefArray.GetLength(1) - 1)
+                        {
+                            Console.WriteLine(benefArray[i, j]);
+                        }
+                        else
+                        {
+                            Console.Write(benefArray[i, j] + "        ");
+                        }
+                    }
+                }
+                return 0;
+            }
+
+            Console.WriteLine(maxBenef([7, 1, 5, 3, 6, 4]));
+        }
+    }
+}
